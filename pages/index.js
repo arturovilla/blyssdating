0d06f3d66573ef03fff06037da7a9912708068f2
arturovilla <acvturi@gmail.com>
2023-04-14 10:56:42 -0500

    added responsive navbar

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import Navbar from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='text-4xl'>
-      Tailwind set up test
+    <div className=''>
+      <Navbar/>
     </div>
   )
 }
