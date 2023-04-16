c1b28eb7872b1df17c23b81ba1e7e6f0e97647c1
arturovilla <acvturi@gmail.com>
2023-04-16 12:15:08 -0500

    added some mvp photos

--- components/Landing.js
+++ components/Landing.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
-import Logo from "../public/logo_t.png"
 import Image from "next/image"
+import Logo from "../public/logo_t.png"
+import Homemvp from "../public/homesmvp.png"
 
 function Landing() {
   return (
     <div className="pt-52 w-full ">
-        <div className=" flex justify-center px-9">
+        <div className=" flex justify-center px-9 ">
             <Link href="#">
                 <Image 
                 src={Logo}
@@ -15,15 +16,24 @@ function Landing() {
                 height="auto"/>
             </Link>
         </div>
-        <div className="text-center flex justify-center pt-5 md:pt-10">
+        <div className="text-center flex justify-center pt-5 md:pt-10 ">
             <h1 className="text-[#4E91CC] text-4xl w-11/12 leading-relaxed md:text-3xl md:w-7/12">
                 Bring life to your soul search with a modern spin on traditional dating!
             </h1>
         </div>
         <div className="text-center flex justify-center items-center pt-5  ">
             <Link href="#">
-                <p className="text-lg bg-[#FFA15C] text-white px-5 py-1 rounded-2xl">Register</p>
+                <p className="text-lg bg-[#FFA15C] text-white px-6 py-1 rounded-2xl">Register</p>
             </Link>
+        </div>
+        <div className="flex justify-center  mt-64">
+            <Image 
+            src={Homemvp}
+            alt="image of the blyss dating app"
+            className="cursor-pointer"
+            width="auto"
+            height="550"/>
+
         </div>
 
     </div>
