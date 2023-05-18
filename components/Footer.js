d8caa721910fd3ab6029a8878ee52ea30c9a3de6
arturovilla <acvturi@gmail.com>
2023-05-18 12:28:59 -0500

    added footer and team

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,25 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTiktok, faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import Link from "next/link"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-[#E3E3E3] flex flex-col items-center justify-center'>
+        <div className="pt-5 w-1/4 flex justify-between">
+            <FontAwesomeIcon icon={faFacebook} size="lg"/>
+            <FontAwesomeIcon icon={faInstagram} size="lg"/>
+            <FontAwesomeIcon icon={faTiktok} size="lg"/>
+            <FontAwesomeIcon icon={faTwitter} size="lg"/>
+        </div>
+        <div className="py-5 flex flex-col items-center">
+            <h3 className="text-[#39698E]">Legal</h3>
+            <div className="flex">
+                <Link href='/' className="text-gray-500">Privacy Policy</Link>
+                <p>  &bull;  </p>
+                <Link href='/'className="text-gray-500">Terms of Use</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar'
 import Landing from '@/components/Landing'
 import Mission from '@/components/Mission'
 import Product from '@/components/Product'
@@ -16,7 +14,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className=''>
-      {/* <Navbar/> */}
 
       <div className='pt-36'>
         <Landing/>
